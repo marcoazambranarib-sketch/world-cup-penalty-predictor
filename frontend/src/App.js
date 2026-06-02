7a045d9aae77dc0f34b76412459938d8663e28c0
Marco Zambrana <marcoazambranarib@gmail.com>
2026-06-01 23:21:43 -0400

    Updated frontend to point to live API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   const getPrediction = async () => {
     try {
-      const res = await fetch('http://127.0.0.1:5000/predict', {
+      const res = await fetch('https://world-cup-penalty-predictor.onrender.com/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ shooter, team, foot, keeper_dive: keeperDive }),
